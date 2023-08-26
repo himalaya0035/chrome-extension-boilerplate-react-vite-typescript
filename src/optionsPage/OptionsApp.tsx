00321b88146b90d47e3863c3d7d388c6dc6ba33d
himalaya0035 <guptahimalaya2@gmail.com>
2023-08-26 20:23:30 +0530

    Support for options page added

---
+++ src/optionsPage/OptionsApp.tsx
@@ -0,0 +1,9 @@
+function OptionsApp() { // Build your options page like any other react app
+  return (
+    <div>
+      Options Page
+    </div>
+  )
+}
+
+export default OptionsApp
---
+++ src/optionsPage/options.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OptionsApp from "./OptionsApp";
+
+ReactDOM.createRoot(document.getElementById('options-root')!).render(
+    <React.StrictMode>
+      <OptionsApp />
+    </React.StrictMode>,
+)
+  
